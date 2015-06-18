ce9e3f5ef6b2a4e67866b1c32c049bb6eaa832a2
Lauren Spencer <spencer.lauren.m@gmail.com>
2015-06-18 10:33:53 +0100

    instagram module setup

--- server.js
+++ server.js
@@ -13,9 +13,32 @@ app.use(bodyParser.json());
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
+instagram.set('client_id', process.env.EXPRESS_PRACTICE_INSHTASHAM_CLIENT_ID);
+instagram.set('client_secret', process.env.EXPRESS_PRACTICE_INSHTASHAM_CLIENT_SECRET);
+
+// console.log(instagram);
+
+// run `ngrok http 3000` and change url below accordingly
+instagram.set('callback_url', 'https://5bb041cb.ngrok.io/callback');
+instagram.set('maxSockets', 10);
+
+instagram.subscriptions.subscribe({
+  object: 'tag',
+  object_id: 'nyc'
+});
 
 router.get('/', function(req, res){
   res.render('index', {header: 'index'});
+});
+
+app.get('/callback', function(req, res){
+  instagram.subscriptions.handshake(req, res);
+  // console.log(req);
+})
+
+app.post('/callback', function(req, res){
+  var data = req.body;
+  console.log(data);
 });
 
 app.use('/', router);
